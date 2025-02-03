f76dba31e397467926a44317a2bb2f677a18b2e5
Johan Hultin <johan.hultin99@gmail.com>
2025-02-03 19:44:53 +0100

    Quick fix

--- Controller-API-SQL/Controllers/ProductController.cs
+++ Controller-API-SQL/Controllers/ProductController.cs
@@ -1,7 +1,6 @@
 ﻿using Controller_API_SQL.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Threading.Tasks;
 
 namespace Controller_API_SQL.Controllers
 {
